39c56c1be9ec636327e95ba14f01e1ee0fc6ae25
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-06-04 11:40:45 -0300

    Updates

---
+++ precache-manifest.9a3033a3b78207825ee0b4822adcc61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee8f6891fe7cc88b4b9687977cabf29",
+    "url": "/artod-site/index.html"
+  },
+  {
+    "revision": "bbcfb1c1f80521f33482",
+    "url": "/artod-site/static/css/main.b6257e75.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/artod-site/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bbcfb1c1f80521f33482",
+    "url": "/artod-site/static/js/main.95947d87.chunk.js"
+  },
+  {
+    "revision": "0b3b22bf170610a93219",
+    "url": "/artod-site/static/js/runtime~main.28f396f7.js"
+  },
+  {
+    "revision": "4397fd824a26f67d48ac15dc0cec645f",
+    "url": "/artod-site/static/media/fondoProducts.4397fd82.png"
+  }
+]);
